0865cf5844bb7dd5562c2af6ae6484711f3ced9b
yurkimus <yurkimus@gmail.com>
2025-01-13 20:05:33 +0100

    Remove specs and up version

--- source/index.d.ts
+++ source/index.d.ts
@@ -1,7 +1,6 @@
 /// <reference path="globals.d.ts" />
 /// <reference path="results.d.ts" />
 /// <reference path="rows.d.ts" />
-/// <reference path="specs.d.ts" />
 
 export * from './enumerations/features.js'
 export * from './enumerations/kinds.js'
