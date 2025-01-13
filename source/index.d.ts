9cbe4476fffefcd6ac5941d0d56998059fbb7680
yurkimus <yurkimus@gmail.com>
2025-01-13 19:15:23 +0100

    Reference global types in index module

--- source/index.d.ts
+++ source/index.d.ts
@@ -1,6 +1,10 @@
+/// <reference path="globals.d.ts" />
+/// <reference path="results.d.ts" />
+/// <reference path="rows.d.ts" />
+/// <reference path="specs.d.ts" />
+
 export * from './enumerations/features.js'
 export * from './enumerations/kinds.js'
 export * from './enumerations/networks.js'
 export * from './enumerations/phases.js'
 export * from './functions/request.js'
-
