20aca4c51600151d9d7482c87e40fa4a6001f36d
yurkimus <yurkimus@gmail.com>
2025-01-15 13:21:26 +0100

    Add GameStatuses enum

---
+++ source/enumerations/game-statuses.js
@@ -0,0 +1,11 @@
+export let GameStatuses = {
+  Approved: 'APPROVED',
+  Archive: 'ARCHIVE',
+  Closed: 'CLOSED',
+  Finished: 'FINISHED',
+  Invitation: 'FORINVITES',
+  Moderation: 'MODERATION',
+  Published: 'PUBLISHED',
+  Rejected: 'REJECTED',
+  Reserved: 'IS_RESERVE',
+}
