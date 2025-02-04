17f5adea53c538884d0d3978af86c24d7d1d6990
yurkimus <yurkimus@gmail.com>
2025-02-04 13:54:17 +0100

    Add Patterns .d.ts

---
+++ source/enumerations/patterns.d.ts
@@ -0,0 +1,38 @@
+import { Features } from './features'
+
+export let Patterns = {
+  /**
+   * Users
+   */
+  [Features.Checkin]: URLPattern,
+
+  [Features.User]: URLPattern,
+
+  [Features.Users]: URLPattern,
+
+  /**
+   * Games
+   */
+  [Features.Theme]: URLPattern,
+
+  [Features.Themes]: URLPattern,
+
+  [Features.Game]: URLPattern,
+
+  [Features.Games]: URLPattern,
+
+  [Features.Registration]: URLPattern,
+
+  [Features.Registrations]: URLPattern,
+
+  /**
+   * Locations
+   */
+  [Features.City]: URLPattern,
+
+  [Features.Cities]: URLPattern,
+
+  [Features.Location]: URLPattern,
+
+  [Features.Locations]: URLPattern,
+}
