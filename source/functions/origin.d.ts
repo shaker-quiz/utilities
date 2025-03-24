4c89f5b12ecc4162fea4b3a0efa37fe5a0fff94a
yurkimus <yurkimus@gmail.com>
2025-03-24 22:02:05 +0100

    Bump version to 0.2.46; update package version in package.json and package-lock.json, and modify type definition for setFeatureNetworkOrigins function

--- source/functions/origin.d.ts
+++ source/functions/origin.d.ts
@@ -1,1 +1,1 @@
-export function setFeatureNetworkOrigins(origins: Record<Service, Record<Network, URL["origin"]>>): void;
+export function setFeatureNetworkOrigins(origins: Partial<Record<Service, Partial<Record<Network, URL["origin"]>>>>): void;
