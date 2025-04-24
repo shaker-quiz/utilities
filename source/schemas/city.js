5707acd4460941f90591c2ad8fd82b767fbfba01
yurkimus <yurkimus@gmail.com>
2025-04-24 14:20:00 +0200

    Add CityColumnDescriptors

--- source/schemas/city.js
+++ source/schemas/city.js
@@ -42,3 +42,265 @@ export var CityColumns = /** @type {const} */ ({
   vk_link: 'vk_link',
   yandex_metrica: 'yandex_metrica',
 })
+
+export var CityColumnDescriptors = /** @type {const} */ ({
+  [CityColumns.address]: {
+    type: 'String',
+    nullable: true,
+    minLength: 1,
+    maxLength: 255,
+  },
+
+  [CityColumns.alias]: {
+    type: 'String',
+    minLength: 1,
+    maxLength: 255,
+  },
+
+  [CityColumns.chatapp_category]: {
+    type: 'String',
+    nullable: true,
+  },
+
+  [CityColumns.chatapp_legacy]: {
+    type: 'Boolean',
+    nullable: true,
+  },
+
+  [CityColumns.chatapp_line]: {
+    type: 'String',
+    nullable: true,
+    minLength: 1,
+    maxLength: 255,
+  },
+
+  [CityColumns.chatapp_tag]: {
+    type: 'String',
+    nullable: true,
+    minLength: 1,
+    maxLength: 255,
+  },
+
+  [CityColumns.chatapp_user]: {
+    type: 'String',
+    nullable: true,
+    minLength: 1,
+    maxLength: 255,
+  },
+
+  [CityColumns.country]: {
+    type: 'String',
+    nullable: true,
+    minLength: 2,
+    maxLength: 2,
+  },
+
+  [CityColumns.currency]: {
+    type: 'String',
+    minLength: 3,
+    maxLength: 3,
+  },
+
+  [CityColumns.custom_html]: {
+    type: 'String',
+    nullable: true,
+  },
+
+  [CityColumns.custom_script]: {
+    type: 'String',
+    nullable: true,
+  },
+
+  [CityColumns.description]: {
+    type: 'String',
+    nullable: true,
+    minLength: 1,
+    maxLength: 255,
+  },
+
+  [CityColumns.email]: {
+    type: 'String',
+    nullable: true,
+    minLength: 1,
+    maxLength: 255,
+    format: 'email',
+  },
+
+  [CityColumns.game_time]: {
+    type: 'String',
+    format: 'time',
+  },
+
+  [CityColumns.id]: {
+    type: 'String',
+    nullable: true,
+    format: 'uuid',
+  },
+
+  [CityColumns.inst_comment]: {
+    type: 'String',
+    nullable: true,
+    deprecated: true,
+  },
+
+  [CityColumns.inst_link]: {
+    type: 'String',
+    nullable: true,
+    deprecated: true,
+  },
+
+  [CityColumns.inst_login]: {
+    type: 'String',
+    nullable: true,
+    deprecated: true,
+  },
+
+  [CityColumns.inst_password]: {
+    type: 'String',
+    nullable: true,
+    deprecated: true,
+  },
+
+  [CityColumns.is_default]: {
+    type: 'Boolean',
+    nullable: true,
+  },
+
+  [CityColumns.is_franchise]: {
+    type: 'Boolean',
+  },
+
+  [CityColumns.max_members_count]: {
+    type: 'Number',
+    nullable: true,
+    format: 'integer',
+  },
+
+  [CityColumns.meta_description]: {
+    type: 'String',
+    nullable: true,
+    minLength: 1,
+    maxLength: 255,
+  },
+
+  [CityColumns.meta_title]: {
+    type: 'String',
+    nullable: true,
+    minLength: 1,
+    maxLength: 255,
+  },
+
+  [CityColumns.min_members_count]: {
+    type: 'Number',
+    nullable: true,
+    format: 'integer',
+  },
+
+  [CityColumns.name]: {
+    type: 'String',
+    minLength: 1,
+    maxLength: 255,
+  },
+
+  [CityColumns.phone]: {
+    type: 'String',
+    nullable: true,
+    minLength: 1,
+    maxLength: 255,
+  },
+
+  [CityColumns.price]: {
+    type: 'String',
+    nullable: true,
+    format: 'float',
+  },
+
+  [CityColumns.region]: {
+    type: 'String',
+    nullable: true,
+    minLength: 1,
+    maxLength: 255,
+  },
+
+  [CityColumns.telegram_chat_id]: {
+    type: 'String',
+    nullable: true,
+    minLength: 1,
+    maxLength: 255,
+  },
+
+  [CityColumns.tg_comment]: {
+    type: 'String',
+    nullable: true,
+    minLength: 1,
+    maxLength: 255,
+  },
+
+  [CityColumns.tg_link]: {
+    type: 'String',
+    nullable: true,
+    minLength: 1,
+    maxLength: 255,
+  },
+
+  [CityColumns.tg_login]: {
+    type: 'String',
+    nullable: true,
+    deprecated: true,
+    minLength: 1,
+    maxLength: 255,
+  },
+
+  [CityColumns.tg_password]: {
+    type: 'String',
+    nullable: true,
+    deprecated: true,
+    minLength: 1,
+    maxLength: 255,
+  },
+
+  [CityColumns.time_created]: {
+    type: 'String',
+    nullable: true,
+    format: 'Date',
+  },
+
+  [CityColumns.time_updated]: {
+    type: 'String',
+    nullable: true,
+    format: 'Date',
+  },
+
+  [CityColumns.timezone]: {
+    type: 'String',
+    nullable: true,
+    format: 'integer',
+  },
+
+  [CityColumns.title]: {
+    type: 'String',
+    nullable: true,
+    minLength: 1,
+    maxLength: 255,
+  },
+
+  [CityColumns.vk_comment]: {
+    type: 'String',
+    nullable: true,
+  },
+
+  [CityColumns.vk_group_id]: {
+    type: 'String',
+    nullable: true,
+  },
+
+  [CityColumns.vk_link]: {
+    type: 'String',
+    nullable: true,
+  },
+
+  [CityColumns.yandex_metrica]: {
+    type: 'String',
+    nullable: true,
+  },
+})
