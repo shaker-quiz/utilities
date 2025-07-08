d0291d30d4c171473d09cfc091b4abc7d23cc917
yurkimus <yurkimus@gmail.com>
2025-07-08 18:02:17 +0200

    Refine types

--- source/globals.d.ts
+++ source/globals.d.ts
@@ -22,42 +22,44 @@ type UseState<Type> = [
  * Core
  */
 
-type Method = typeof import('./enumerations/core/methods.js').Methods[number]
+type Method = (typeof import('./enumerations/core/methods.js').Methods)[number]
 
-type Domain = typeof import('./enumerations/core/features.js').Domains[number]
+type Domain = (typeof import('./enumerations/core/features.js').Domains)[number]
 
 type Procedure =
-  typeof import('./enumerations/core/features.js').Procedures[number]
+  (typeof import('./enumerations/core/features.js').Procedures)[number]
 
 type Feature = Domain | Procedure
 
-type Kind = typeof import('./enumerations/core/kinds.js').Kinds[number]
+type Kind = (typeof import('./enumerations/core/kinds.js').Kinds)[number]
 
 type Scope = `${Method}/${Feature}/${Kind}`
 
-type Requirement =
-  typeof import('./enumerations/misc/requirements.js').Requirements[number]
-
-type Role = keyof typeof import('./enumerations/entities/roles.js').Roles
+type Role = (typeof import('./enumerations/entities/roles.js').Roles)[number]
 
 type GameStatus =
-  keyof typeof import('./enumerations/entities/game-statuses.js').GameStatuses
+  (typeof import('./enumerations/entities/game-statuses.js').GameStatuses)[
+    number
+  ]
 
 type RegistrationStatus =
-  keyof typeof import('./enumerations/entities/registration-statuses.js').RegistrationStatuses
+  (typeof import('./enumerations/entities/registration-statuses.js').RegistrationStatuses)[
+    number
+  ]
 
-type Lineup = keyof typeof import('./enumerations/entities/lineups.js').Lineups
+type Lineup =
+  (typeof import('./enumerations/entities/lineups.js').Lineups)[number]
 
 type Affilation =
-  keyof typeof import('./enumerations/entities/affilations.js').Affilations
+  (typeof import('./enumerations/entities/affilations.js').Affilations)[number]
 
 /**
  * Misc
  */
 
-type Icon = typeof import('./enumerations/misc/icons.js').Icons[number]
+type Icon = (typeof import('./enumerations/misc/icons.js').Icons)[number]
 
-type Phase = typeof import('./enumerations/misc/phases.js').Phases[number]
+type Phase = (typeof import('./enumerations/misc/phases.js').Phases)[number]
 
 /**
  * Products
