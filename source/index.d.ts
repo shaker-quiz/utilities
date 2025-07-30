b82a1316ab0ddf44246fbc2b034f61f813cba475
yurkimus <yurkimus@gmail.com>
2025-07-30 23:07:52 +0200

    Revert references

--- source/index.d.ts
+++ source/index.d.ts
@@ -3,19 +3,19 @@
 /// <reference path="./scope.d.ts" />
 /// <reference path="./server.d.ts" />
 
-/// <reference path="./enumerations/core/features.d.ts" />
-/// <reference path="./enumerations/core/kinds.d.ts" />
-/// <reference path="./enumerations/core/methods.d.ts" />
-/// <reference path="./enumerations/core/scopes.d.ts" />
+export * from './enumerations/core/features.d.ts'
+export * from './enumerations/core/kinds.d.ts'
+export * from './enumerations/core/methods.d.ts'
+export * from './enumerations/core/scopes.d.ts'
 
-/// <reference path="./enumerations/misc/constants.d.ts" />
-/// <reference path="./enumerations/misc/cookies.d.ts" />
-/// <reference path="./enumerations/misc/icons.d.ts" />
-/// <reference path="./enumerations/misc/phases.d.ts" />
-/// <reference path="./enumerations/misc/regexps.d.ts" />
+export * from './enumerations/misc/constants.d.ts'
+export * from './enumerations/misc/cookies.d.ts'
+export * from './enumerations/misc/icons.d.ts'
+export * from './enumerations/misc/phases.d.ts'
+export * from './enumerations/misc/regexps.d.ts'
 
-/// <reference path="./enumerations/entities/affilations.d.ts" />
-/// <reference path="./enumerations/entities/game-statuses.d.ts" />
-/// <reference path="./enumerations/entities/lineups.d.ts" />
-/// <reference path="./enumerations/entities/registration-statuses.d.ts" />
-/// <reference path="./enumerations/entities/roles.d.ts" />
+export * from './enumerations/entities/affilations.d.ts'
+export * from './enumerations/entities/game-statuses.d.ts'
+export * from './enumerations/entities/lineups.d.ts'
+export * from './enumerations/entities/registration-statuses.d.ts'
+export * from './enumerations/entities/roles.d.ts'
