ce2036cdbbfccf363faa04533947289d7d1a2424
yurkimus <yurkimus@gmail.com>
2025-09-11 15:47:07 +0200

    Refine venue status types

--- source/enumerations/entities/venue-statuses.js
+++ source/enumerations/entities/venue-statuses.js
@@ -5,25 +5,21 @@ export var VenueStatus = /** @type {const} */ ({
 
 export var VenueStatuses = Object.values(VenueStatus)
 
-/** @type {Record<VenueStatus, number>} */
 export var VenueStatusWeight = {
   [VenueStatus.Active]: 0,
   [VenueStatus.Archive]: 1,
 }
 
-/** @type {Record<VenueStatus, string>} */
 export var VenueStatusTitle = {
   [VenueStatus.Active]: 'В пользовании',
   [VenueStatus.Archive]: 'В архиве',
 }
 
-/** @type {Record<VenueStatus, string>} */
 export var VenueStatusColor = {
   [VenueStatus.Active]: 'success',
   [VenueStatus.Archive]: 'current',
 }
 
-/** @type {Record<VenueStatus, string>} */
 export var VenueStatusTextColor = {
   [VenueStatus.Active]: 'text-success',
   [VenueStatus.Archive]: 'text-current',
