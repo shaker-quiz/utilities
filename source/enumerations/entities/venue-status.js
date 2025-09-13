8a52415f736c92c968d4445aeeb032cef5f80a09
yurkimus <yurkimus@gmail.com>
2025-09-13 23:11:07 +0200

    Fix case for venue enums

--- source/enumerations/entities/venue-status.js
+++ source/enumerations/entities/venue-status.js
@@ -1,32 +1,32 @@
 export var VenueStatus = /** @type {const} */ ({
-  'Active': 'Active',
-  'Archive': 'Archive',
+  'ACTIVE': 'ACTIVE',
+  'ARCHIVE': 'ARCHIVE',
 })
 
 export var VenueStatuses = Object.values(VenueStatus)
 
 export var VenueStatusWeight = {
-  [VenueStatus.Active]: 0,
-  [VenueStatus.Archive]: 1,
+  [VenueStatus.ACTIVE]: 0,
+  [VenueStatus.ARCHIVE]: 1,
 }
 
 export var VenueStatusTitle = {
-  [VenueStatus.Active]: 'В пользовании',
-  [VenueStatus.Archive]: 'В архиве',
+  [VenueStatus.ACTIVE]: 'В пользовании',
+  [VenueStatus.ARCHIVE]: 'В архиве',
 }
 
 export var VenueStatusColor = {
-  [VenueStatus.Active]: 'success',
-  [VenueStatus.Archive]: 'current',
+  [VenueStatus.ACTIVE]: 'success',
+  [VenueStatus.ARCHIVE]: 'current',
 }
 
 export var VenueStatusTextColor = {
-  [VenueStatus.Active]: 'text-success',
-  [VenueStatus.Archive]: 'text-current',
+  [VenueStatus.ACTIVE]: 'text-success',
+  [VenueStatus.ARCHIVE]: 'text-current',
 }
 
 /** @type {Record<VenueStatus, Icon>} */
 export var VenueStatusIcon = {
-  [VenueStatus.Active]: 'hero/outline/check',
-  [VenueStatus.Archive]: 'hero/outline/archive-box',
+  [VenueStatus.ACTIVE]: 'hero/outline/check',
+  [VenueStatus.ARCHIVE]: 'hero/outline/archive-box',
 }
