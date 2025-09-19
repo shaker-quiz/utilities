e65a84efcc1292dc796ceb18316d189d53b85be1
yurkimus <yurkimus@gmail.com>
2025-09-19 13:13:14 +0200

    Adjust CityAffilation enum

--- source/enumerations/entities/city-affilation.js
+++ source/enumerations/entities/city-affilation.js
@@ -3,9 +3,40 @@ export var CityAffilation = /** @type {const} */ ({
   'Franchise': 'Franchise',
 })
 
+export var CityAffilations = Object.values(CityAffilation)
+
 export var CityAffilationWeight = {
   [CityAffilation.Branch]: 0,
   [CityAffilation.Franchise]: 1,
 }
 
-export var CityAffilations = Object.values(CityAffilation)
+export var CityAffilationTitle = {
+  [CityAffilation.Branch]: 'Филиал',
+  [CityAffilation.Franchise]: 'Франшиза',
+}
+
+export var CityAffilationActionTitle = {
+  [CityAffilation.Branch]: 'Назначить филиалом',
+  [CityAffilation.Franchise]: 'Назначить франшизой',
+}
+
+export var CityAffilationActionDescpription = {
+  [CityAffilation.Branch]: 'Изменить тип на: Филиал',
+  [CityAffilation.Franchise]: 'Изменить тип на: Франшиза',
+}
+
+export var CityAffilationColor = {
+  [CityAffilation.Branch]: 'primary',
+  [CityAffilation.Franchise]: 'secondary',
+}
+
+export var CityAffilationTextColor = {
+  [CityAffilation.Branch]: 'text-primary',
+  [CityAffilation.Franchise]: 'text-secondary',
+}
+
+/** @type {Record<CityAffilation, Icon>} */
+export var CityAffilation = {
+  [CityAffilation.Branch]: 'hero/outline/building-office-2',
+  [CityAffilation.Franchise]: 'hero/outline/building-storefront',
+}
