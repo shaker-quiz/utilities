482959d392eba153ec3b6c938adf3ac198aaaef8
yurkimus <yurkimus@gmail.com>
2025-10-17 12:57:53 +0200

    Refine StateKey

--- source/enumerations/misc/regexps.js
+++ source/enumerations/misc/regexps.js
@@ -7,6 +7,9 @@ export var RegExps = {
 export var StateKey = {
   'user': /^GET\/user\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,
   'role': /^GET\/role\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,
+  'country': /^GET\/country\/[A-Z]{2}$/,
+  'currency': /^GET\/currency\/.+$/,
+  'timezone': /^GET\/timezone\/[A-Za-z_]+\/[A-Za-z_]+$/,
   'city': /^GET\/city\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,
   'venue': /^GET\/venue\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,
   'theme': /^GET\/theme\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,
