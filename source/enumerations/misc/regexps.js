4f4eedd0f87cbf1ca94c9bd4ad90fb9e9a59522e
yurkimus <yurkimus@gmail.com>
2025-10-30 19:33:22 +0100

    Add StateKey

--- source/enumerations/misc/regexps.js
+++ source/enumerations/misc/regexps.js
@@ -12,6 +12,8 @@ export var StateKey = {
   'currency': /^GET\/currency\/.+$/,
   'timezone': /^GET\/timezone\/[A-Za-z_]+\/[A-Za-z_]+$/,
   'city': /^GET\/city\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,
+  'city/venue':
+    /^GET\/city\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\/venue\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,
   'venue': /^GET\/venue\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,
   'theme': /^GET\/theme\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,
   'game': /^GET\/game\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,
