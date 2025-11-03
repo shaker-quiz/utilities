bc15ea8feb7828e4350a33e6370c63f4628ff486
yurkimus <yurkimus@gmail.com>
2025-11-03 17:50:55 +0100

    Add venue status getter

--- source/enumerations/entities/venue-status.js
+++ source/enumerations/entities/venue-status.js
@@ -1,32 +1,43 @@
 export var VenueStatus = /** @type {const} */ ({
   'ACTIVE': 'ACTIVE',
   'ARCHIVE': 'ARCHIVE',
+  'UNKNOWN': 'UNKNOWN',
 })
 
 export var VenueStatuses = Object.values(VenueStatus)
 
 export var VenueStatusWeight = {
-  [VenueStatus.ACTIVE]: 0,
-  [VenueStatus.ARCHIVE]: 1,
+  [VenueStatus['ACTIVE']]: 0,
+  [VenueStatus['ARCHIVE']]: 1,
 }
 
 export var VenueStatusTitle = {
-  [VenueStatus.ACTIVE]: 'Активно',
-  [VenueStatus.ARCHIVE]: 'Архив',
-}
-
-export var VenueStatusColor = {
-  [VenueStatus.ACTIVE]: 'success',
-  [VenueStatus.ARCHIVE]: 'current',
-}
-
-export var VenueStatusTextColor = {
-  [VenueStatus.ACTIVE]: 'text-success',
-  [VenueStatus.ARCHIVE]: 'text-current',
+  [VenueStatus['ACTIVE']]: 'Активно',
+  [VenueStatus['ARCHIVE']]: 'Архив',
+  [VenueStatus['UNKNOWN']]: 'Неизвестно',
 }
 
 /** @type {Record<VenueStatus, Icon>} */
 export var VenueStatusIcon = {
-  [VenueStatus.ACTIVE]: 'hero/outline/check',
-  [VenueStatus.ARCHIVE]: 'hero/outline/archive-box',
+  [VenueStatus['ACTIVE']]: 'hero/outline/check',
+  [VenueStatus['ARCHIVE']]: 'hero/outline/archive-box',
+  [VenueStatus['UNKNOWN']]: 'hero/outline/no-symbol',
+}
+
+var get = value =>
+  value in VenueStatus
+    ? value
+    : VenueStatus['UNKNOWN']
+
+export var getVenueStatus = value => {
+  switch (typeof value) {
+    case 'string':
+      return get(value)
+
+    case 'object':
+      return get(value?.status)
+
+    default:
+      return get(undefined)
+  }
 }
