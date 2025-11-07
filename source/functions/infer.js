c4a1acb5027b3ed816847c94251d4efb2eb34b2c
yurkimus <yurkimus@gmail.com>
2025-11-07 23:35:02 +0100

    Add general infer

---
+++ source/functions/infer.js
@@ -0,0 +1,27 @@
+import { Mode } from '../enumerations/mode.js'
+
+/**
+ * @template {*} E
+ *
+ * @param {E} Enumeration
+ * @param {E[keyof E]} value
+ */
+export var inferKey = (Enumeration, value) =>
+  value in Enumeration
+    ? Enumeration[value]
+    : Mode['Unknown']
+
+/**
+ * @template {*} E
+ *
+ * @param {E} Enumeration
+ * @param {E[keyof E]} value
+ */
+export var guardKey = (Enumeration, value) => {
+  var key = inferKey(Enumeration, value)
+
+  if (key === Mode['Unknown'])
+    throw TypeError(`Cannot infer enum key '${value}'.`)
+
+  return key
+}
