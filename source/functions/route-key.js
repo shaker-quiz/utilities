f7d2e3181eb6bb705e806580ae580985739416b1
yurkimus <yurkimus@gmail.com>
2025-11-08 14:06:08 +0100

    Fix wrong import usage

--- source/functions/route-key.js
+++ source/functions/route-key.js
@@ -1,6 +1,7 @@
-import { inferMethod } from '../enumerations/method.js'
+import { Method } from '../enumerations/method.js'
 import { Mode } from '../enumerations/mode.js'
 import { hydrateRoutePathname } from '../functions/hydrate-route-pathname.js'
+import { guardKey } from '../functions/infer.js'
 
 /**
  * @param {keyof typeof import('@shakerquiz/utilities').Method} maybeMethod
@@ -8,7 +9,7 @@ import { hydrateRoutePathname } from '../functions/hydrate-route-pathname.js'
  * @param {any[]} maybeParameters
  */
 export var routeKey = (maybeMethod, maybeRoute, maybeParameters) => {
-  var method = inferMethod(maybeMethod)
+  var method = guardKey(Method, maybeMethod)
 
   if (method === Mode['Unknown'])
     throw TypeError(`Cannot infer method of '${maybeMethod}'.`)
