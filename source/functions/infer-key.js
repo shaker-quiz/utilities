f957864ea6f367d523b66e5dc24a6cd411587001
yurkimus <yurkimus@gmail.com>
2025-11-10 13:07:04 +0100

    Adjust TypeError message

--- source/functions/infer-key.js
+++ source/functions/infer-key.js
@@ -9,5 +9,5 @@ export var inferKey = (Enumeration, value) => {
   if (value in Enumeration)
     return Enumeration[value]
   else
-    throw TypeError(`Cannot infer enum's '${Object.values(Enumeration).join(', ')}' key for value '${value}'.`)
+    throw TypeError(`Cannot infer key of value: '${value}' for enum: '${Object.values(Enumeration).join(', ')}'.`)
 }
