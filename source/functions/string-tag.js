03a6653bb9cd98cfc4505b1b5ecdee97ad279853
yurkimus <yurkimus@gmail.com>
2025-11-26 03:59:21 +0100

    Add isValue helper

--- source/functions/string-tag.js
+++ source/functions/string-tag.js
@@ -1,3 +1,5 @@
 export const toStringTag = Object.freeze(x => Object.prototype.toString.call(x).slice(8, -1))
 
 export const isStringTag = Object.freeze((tag, x) => tag === Object.prototype.toString.call(x).slice(8, -1))
+
+export const isValue = Object.freeze((x, ...tags) => tags.includes(toStringTag(x)))
