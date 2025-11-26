a70795c2e91da3aac24ab4e341cdfa5aa42e46f9
yurkimus <yurkimus@gmail.com>
2025-11-26 01:39:20 +0100

    Add timezone pattern

--- source/enumerations/pattern.js
+++ source/enumerations/pattern.js
@@ -1,4 +1,5 @@
 export const Pattern = Object.freeze({
   UUID: /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/,
   JWT: /^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$/,
+  ZONE: /^[A-Za-z]+\/(?:[A-Za-z]+(?:_[A-Za-z]+)?)+$/,
 })
