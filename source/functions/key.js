fcb7f8a14d9ae6214e740e2e6eadb69d911e800d
yurkimus <yurkimus@gmail.com>
2025-12-04 20:28:44 +0100

    Add key jsdoc

--- source/functions/key.js
+++ source/functions/key.js
@@ -2,13 +2,33 @@ import { inferMethod } from '../enumerations/method.js'
 
 import { hydrateRoutePathname } from './hydrate-route-pathname.js'
 
-export const tag = Object.freeze((m, r, p) => {
-  var method = inferMethod(m)
+export const tag = Object.freeze(
+  /**
+   * @template {keyof typeof import('../enumerations/method.js').Method} M
+   * @template {keyof typeof import('../enumerations/route.js').Route} R
+   *
+   * @param {M} m
+   * @param {R} r
+   * @param {string[]} p
+   */
+  (m, r, p) => {
+    var method = inferMethod(m)
 
-  if (method === 'Unknown')
-    throw TypeError(`Could not infer method of: '${m}'.`)
+    if (method === 'Unknown')
+      throw TypeError(`Could not infer method of: '${m}'.`)
 
-  return method + '/' + hydrateRoutePathname(r, p)
-})
+    return method + '/' + hydrateRoutePathname(r, p)
+  },
+)
 
-export const tagexp = Object.freeze((m, r, p) => new RegExp(`^${tag(m, r, p)}$`))
+export const tagexp = Object.freeze(
+  /**
+   * @template {keyof typeof import('../enumerations/method.js').Method} M
+   * @template {keyof typeof import('../enumerations/route.js').Route} R
+   *
+   * @param {M} m
+   * @param {R} r
+   * @param {string[]} p
+   */
+  (m, r, p) => new RegExp(`^${tag(m, r, p)}$`),
+)
