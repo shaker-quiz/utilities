2cefcdc8fb1470154709bec90b16e3d906b9291a
yurkimus <yurkimus@gmail.com>
2026-01-18 22:19:34 +0100

    Add PathnameRoute generator

--- scripts/route-pathname.js
+++ scripts/route-pathname.js
@@ -10,13 +10,28 @@ let pathnames = Routes.map(route =>
     .join('/')
 )
 
-let RoutePathnames = JSON.stringify(pathnames, undefined, 2)
+let RoutePathnames = JSON.stringify(
+  pathnames,
+  undefined,
+  2,
+)
 
-let RoutePathname = JSON.stringify(Routes.reduce((o, x, i) => (o[x] = pathnames[i], o), {}), null, 2)
+let RoutePathname = JSON.stringify(
+  Routes.reduce((o, x, i) => (o[x] = pathnames[i], o), {}),
+  null,
+  2,
+)
+
+let PathnameRoute = JSON.stringify(
+  pathnames.reduce((o, x, i) => (o[x] = Routes[i], o), {}),
+  null,
+  2,
+)
 
 Bun.write(
   './source/entities/route-pathname.js',
   template
     .replace('/* RoutePathnames */', RoutePathnames)
-    .replace('/* RoutePathname */', RoutePathname),
+    .replace('/* RoutePathname */', RoutePathname)
+    .replace('/* PathnameRoute */', PathnameRoute),
 )
