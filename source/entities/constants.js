d60d102b9a145de6cc11687cee1e07d9da2c6ab8
Vladislav Ponomarev <2877584+PonomareVlad@users.noreply.github.com>
2026-02-03 00:42:17 +0100

    Add Time constants for time intervals

--- source/entities/constants.js
+++ source/entities/constants.js
@@ -4,3 +4,14 @@ export const Constants = Object.freeze({
    */
   NilUuid: '00000000-0000-0000-0000-000000000000',
 })
+
+export const Time = Object.freeze(
+  /** @type {const} */ ({
+    Millisecond: 1,
+    Second: 1_000,
+    Minute: 60_000,
+    Hour: 3_600_000,
+    Day: 86_400_000,
+    Week: 604_800_000,
+  }),
+)
