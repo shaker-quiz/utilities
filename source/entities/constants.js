d13c7f0e9587d19b255b6596cfe88c54e0fb1645
Iurii <51003204+yurkimus@users.noreply.github.com>
2026-02-03 02:05:53 +0100

    Refactor time constants in constants.js
    Removed the Time object and added individual time constants.

--- source/entities/constants.js
+++ source/entities/constants.js
@@ -3,15 +3,9 @@ export const Constants = Object.freeze({
    * @see https://www.rfc-editor.org/rfc/rfc9562.html#name-nil-uuid
    */
   NilUuid: '00000000-0000-0000-0000-000000000000',
+  MillisecondsInSecond: 1_000,
+  MillisecondsInMinute: 60_000,
+  MillisecondsInHour: 3_600_000,
+  MillisecondsInDay: 86_400_000,
+  MillisecondsInWeek: 604_800_000
 })
-
-export const Time = Object.freeze(
-  /** @type {const} */ ({
-    Millisecond: 1,
-    Second: 1_000,
-    Minute: 60_000,
-    Hour: 3_600_000,
-    Day: 86_400_000,
-    Week: 604_800_000,
-  }),
-)
