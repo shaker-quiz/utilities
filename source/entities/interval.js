f79c48b5488bcf10e8a75e12dd3c06baf43dd976
yurkimus <yurkimus@gmail.com>
2026-02-17 00:57:12 +0100

    Add interval

---
+++ source/entities/interval.js
@@ -0,0 +1,9 @@
+export const Interval = Object.freeze({
+  '1 month': '1 month',
+  '3 months': '3 months',
+  '6 months': '6 months',
+  '1 year': '1 year',
+  'all time': 'all time',
+})
+
+export const Intervals = Object.freeze(Object.values(Interval))
---
+++ source/entities/registration-interval.js
@@ -0,0 +1,21 @@
+import { Interval } from './interval.js'
+
+export const CategoryRegistrationIntervals = Object.freeze({
+  [Interval['1 month']]: Object.freeze([
+    Interval['1 month'],
+  ]),
+
+  [Interval['3 months']]: Object.freeze([
+    Interval['3 months'],
+  ]),
+
+  [Interval['6 months']]: Object.freeze([
+    Interval['6 months'],
+  ]),
+
+  [Interval['1 year']]: Object.freeze([
+    Interval['1 year'],
+  ]),
+
+  [Interval['all time']]: Object.freeze([]),
+})
