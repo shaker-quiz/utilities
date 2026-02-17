88927d591e63f814cb36e92fccc17eb6cfe2a75b
yurkimus <yurkimus@gmail.com>
2026-02-17 02:23:00 +0100

    Add more intervals

--- source/entities/interval.js
+++ source/entities/interval.js
@@ -1,4 +1,7 @@
 export const Interval = Object.freeze({
+  '1 day': '1 day',
+  '3 days': '3 days',
+  '1 week': '1 week',
   '1 month': '1 month',
   '3 months': '3 months',
   '6 months': '6 months',
