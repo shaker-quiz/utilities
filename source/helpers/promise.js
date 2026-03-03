1e2bc124215e778a1da0bb8ef64ff5ae58e543c6
yurkimus <yurkimus@gmail.com>
2026-03-03 20:51:05 +0100

    promise helpers @type const

--- source/helpers/promise.js
+++ source/helpers/promise.js
@@ -2,14 +2,14 @@ export let settle = value =>
   Promise
     .resolve(value)
     .then(
-      value => ({ status: 'fulfilled', value }),
-      reason => ({ status: 'rejected', reason }),
+      value => (/** @type {const} */ ({ status: 'fulfilled', value })),
+      reason => (/** @type {const} */ ({ status: 'rejected', reason })),
     )
 
 export let trySettle = (fn, ...args) =>
   Promise
     .try(fn, ...args)
     .then(
-      value => ({ status: 'fulfilled', value }),
-      reason => ({ status: 'rejected', reason }),
+      value => (/** @type {const} */ ({ status: 'fulfilled', value })),
+      reason => (/** @type {const} */ ({ status: 'rejected', reason })),
     )
