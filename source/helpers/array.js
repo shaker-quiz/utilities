65c987b03e78ab582481fea9361e6c5bb60d7843
yurkimus <yurkimus@gmail.com>
2026-03-13 00:16:23 +0100

    filterUniqueBy

--- source/helpers/array.js
+++ source/helpers/array.js
@@ -1,1 +1,4 @@
 export const filterUnique = (value, index, array) => array.indexOf(value) === index
+
+export const filterUniqueBy = predicate => (value, index, array) =>
+  array.findIndex(v => predicate(v) === predicate(value)) === index
