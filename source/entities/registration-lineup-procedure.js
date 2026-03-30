de997ecc45edba15efc474d1467364b92f99c20e
yurkimus <yurkimus@gmail.com>
2026-03-30 17:09:39 +0200

    Add registratio-{status,lineup}-procedure entries

---
+++ source/entities/registration-lineup-procedure.js
@@ -0,0 +1,7 @@
+export const RegistrationLineupProcedure = /** @type {const} */ ({
+  'Assignment': 'Assignment',
+  'Promotion': 'Promotion',
+  'Reservation': 'Reservation',
+})
+
+export const RegistrationLineupProcedures = Object.values(RegistrationLineupProcedure)
